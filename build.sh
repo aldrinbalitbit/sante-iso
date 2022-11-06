2f784882787d8e89f77fc463b927b43634558b60
aldrinbalitbit <aldrinbalitbit1127@gmail.com>
2022-11-06 10:50:07 +0800

    Using hover source

--- build.sh
+++ build.sh
@@ -8,19 +8,42 @@ thank_you () {
 	echo "Thanks for building Sante ISO!"
 }
 
+configure_cmd () {
+	./configure --prefix=/usr \
+		    --host=x86_64-sante-gnu \
+		    --build=x86_64-pc-linux-gnu \
+		    $@
+}
+
+make_cmd () {
+	make -j$(nproc)
+	make -j$(nproc) $1 DESTDIR=/rootfs
+}
+
 # Build dependencies
 sudo apt-get update -qqy
 sudo apt-get install gcc g++ make
 
 # Create rootfs directory
-mkdir rootfs
+mkdir /rootfs
 
 # Create root file system directories and merge /usr
-cd rootfs
-mkdir -p {boot,home,mnt,opt,srv,etc,var,run,dev,sys,proc,root,usr/{bin,lib,share,src}}
+cd /rootfs
+mkdir -p {boot,home,mnt,opt,srv,etc,var/tmp,tmp,run,dev,sys,proc,root,usr/{bin,lib,share,src,local/{bin,lib}}
 ln -sf usr/bin bin
 ln -sf usr/bin sbin
 ln -sf usr/lib lib
 ln -sf usr/lib lib64
 ln -sf sbin usr/bin
 ln -sf lib64 usr/lib
+ln -sf sbin usr/local/bin
+ln -sf lib64 usr/local/lib
+cd $HOME/sante-iso
+
+# Source build for rootfs
+# GNU C Library
+wget -qO- https://ftp.gnu.org/gnu/glibc/glibc-2.36.tar.gz | tar -xJpf -
+cd glibc-2.36
+configure_cmd CFLAGS="-w -g -O2"
+make_cmd install
+cd ..
